49cb7decf039ac1c2573e70312081dac128deb35
wawan setiawan <wawan.14set@gmail.com>
2022-10-29 13:07:15 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/landing-page";
+import CariMobil from "./pages/cari-mobil";
+import Detail from "./pages/detail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cari-mobil" element={<CariMobil />} />
+        <Route path="/Detail/:id" element={<Detail />} />
+      </Routes>
+    </>
   );
 }
 
